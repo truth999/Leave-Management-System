a03356fe00ef2a4716e1038ea85337f5c7091300
truth999 <truthdev999@gmail.com>
2020-07-26 12:27:55 +0530

    Add files via upload

---
+++ LeaveManagementWebApi/EmployeeWebApiController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LeaveManagementModels;
+using LeaveManagementManager;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeWebApiController : ApiController
+    {
+        [Route("api/EmployeeWebApi/EmployeeDetails")]
+        [HttpGet]
+        public RegisterModel EmployeeDetails(string username)
+        {
+            EmployeeManager employeeManager = new EmployeeManager();
+            return employeeManager.EmployeeDetails(username);
+        }
+
+        [Route("api/EmployeeWebApi/ApplyForLeaveDetails")]
+        [HttpPost]
+        public void ApplyForLeaveDetails(LeaveModel leaveModel)
+        {
+            EmployeeManager employeeManager = new EmployeeManager();
+            employeeManager.LeaveDetails(leaveModel);
+        }
+
+        [Route("api/EmployeeWebApi/GetLeaveDetails")]
+        [HttpGet]
+        public List<LeaveModel> GetLeaveDetails(int empid)
+        {
+            
+            EmployeeManager employeeManager = new EmployeeManager();
+            return employeeManager.GetLeaveDetails(empid);
+
+        }
+
+        [Route("api/EmployeeWebApi/UpdateLeaveBalance")]
+        [HttpPost]
+        public void UpdateLeaveBalance()
+        {
+            EmployeeManager employeeManager = new EmployeeManager();
+            employeeManager.UpdateLeaveBalance();
+        }
+    }
+}
---
+++ LeaveManagementWebApi/LoginWebApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LeaveManagementModels;
+using LeaveManagementManager;
+
+namespace WebApi.Controllers
+{
+    public class LoginWebApiController : ApiController
+    {
+        [Route("api/LoginWebApi/RegisterEmployee")]
+        [HttpPost]
+        public void RegisterEmployee(RegisterModel register)
+        {
+            RegisterManager registerManager = new RegisterManager();
+            registerManager.ManageRegister(register);
+        }
+    }
+}
