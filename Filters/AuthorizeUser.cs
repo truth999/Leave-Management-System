4393bdb7eacc4146b518a1f54cdb51647dc57938
truth999 <truthdev999@gmail.com>
2020-08-20 19:05:01 +0530

    Add files via upload

---
+++ Filters/AuthorizeUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Leave_Management.Filters
+{
+    public class AuthorizeUser : System.Web.Mvc.ActionFilterAttribute, System.Web.Mvc.IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if(HttpContext.Current.Session["usertype"] == null)
+            {
+                filterContext.Result = new System.Web.Mvc.RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary
+                {
+                    {"Controller","Login"},
+                    {"Action","Login" }
+                });
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
