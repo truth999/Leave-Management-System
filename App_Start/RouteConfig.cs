2e53ed5917d7fecaca86299f5357567d213bca75
truth999 <truthdev999@gmail.com>
2020-08-20 18:45:21 +0530

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Leave_Management
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Login",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Login", action = "Login" }
+                );
+
+            //routes.MapRoute(
+            //    name: "Register",
+            //    url: "{controller}/{action}",
+            //    defaults: new { controller = "Login", action = "Register" }
+            //    );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
